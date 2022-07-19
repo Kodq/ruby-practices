68b1641ccee9b3681896df33910aeaf8df6627c7
Koda <s_riku@shiolab.com>
2022-07-19 20:15:35 +0900

    インデントや書き方の修正

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,14 +1,13 @@
-range = 1 .. 20
+range = (1..20)
 
-range.each{|num|
-    if num % (3 * 5) == 0
-        puts("FizzBuzz")
-    elsif num % 3 == 0
-        puts("Fizz") 
-    elsif num % 5 == 0
-        puts("Buzz") 
-    else
-        puts(num)
-   end
-
-}
+range.each do |num|
+  if num % (3 * 5) == 0
+    puts("FizzBuzz")
+  elsif num % 3 == 0
+    puts("Fizz") 
+  elsif num % 5 == 0
+    puts("Buzz") 
+  else
+    puts(num)
+  end
+end

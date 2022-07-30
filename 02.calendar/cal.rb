aa57da05003be71c613b30799e1ccd24d02affd4
Koda <s_riku@shiolab.com>
2022-07-30 19:56:16 +0900

    修正2

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,24 +4,24 @@ require 'optparse'
 params = ARGV.getopts("","y:#{Date.today.year}", "m:#{Date.today.month}")
 firstday = Date.new(params["y"].to_i,params["m"].to_i,1)
 lastday = Date.new(params["y"].to_i,params["m"].to_i,-1)
-range = (firstday.mday..lastday.mday)
+range = (firstday..lastday)
 space = "   "
 
 puts("#{params["m"]}月 #{params["y"]}".center(20))
 puts("日 月 火 水 木 金 土")
 
-6.times do |t|
+6.times do |t| 
   if t == firstday.wday.to_i
-    print(space * t)
+    print(space * firstday.wday.to_i)
     break
   end 
 end
 
 range.each do |day|
-  printf("%2d",day)
+  printf("%2d",day.mday)
   print(" ")
-  if Date.new(params["y"].to_i,params["m"].to_i,day).wday == 6
-    puts()
+  if Date.new(params["y"].to_i,params["m"].to_i,day.mday).wday == 6
+    puts
   end
 end
 

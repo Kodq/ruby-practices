6ad7b2ae52005e5c36abcb4deeebe2d9149575c0
Koda <s_riku@shiolab.com>
2022-08-08 09:20:41 +0900

    rubocopを通してコードを改善しました。

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,45 +1,44 @@
-score = ARGV[0]
-scores = score.split(',')
-$shots = []
-$frames = []
+# frozen_string_literal: true
 
-def char_to_num (scores)
+def char_to_num(scores)
+  shots = []
   scores.each do |s|
     if s == 'X'
-      $shots << 10
-      $shots << 0
+      shots << 10
+      shots << 0
     else
-      $shots << s.to_i
+      shots << s.to_i
     end
   end
+  shots
 end
 
-def split_frames (shots)
+def split_frames(shots)
+  frames = []
   shots.each_slice(2) do |s|
-    $frames << s
+    frames << s
   end
-end  
-
+  frames
+end
 
 def sort_frames(frames)
   i = 0
 
   frames.each do
-    if frames[i][0] == 10
-      frames[i].pop
-    end
+    frames[i].pop if frames[i][0] == 10
     i += 1
   end
 
-  if frames.size == 11
+  case frames.size
+  when 11
     frames[9].concat(frames.pop)
-  elsif frames.size == 12
-    2.times do 
+  when 12
+    2.times do
       frames[9].concat(frames.pop)
     end
   end
-end  
-
+  frames
+end
 
 def score_cal(frames)
   j = 0
@@ -48,10 +47,10 @@ def score_cal(frames)
 
   while j <= 8
     if frames[j][0] == 10
-    sum += frames.flatten[num] + frames.flatten[num + 1] + frames.flatten[num + 2]
-    num += 1
+      sum += frames.flatten[num] + frames.flatten[num + 1] + frames.flatten[num + 2]
+      num += 1
     elsif frames[j].sum == 10
-      sum += frames[j].sum + frames[j+1][0]
+      sum += frames[j].sum + frames[j + 1][0]
       num += 2
     else
       sum += frames[j].sum
@@ -61,11 +60,12 @@ def score_cal(frames)
   end
 
   sum += frames[j].sum
-
   puts sum
 end
 
-char_to_num(scores)
-split_frames($shots)
-sort_frames($frames)
-score_cal($frames)
+score = ARGV[0]
+scores = score.split(',')
+
+shots = char_to_num(scores)
+frames = sort_frames(split_frames(shots))
+score_cal(frames)

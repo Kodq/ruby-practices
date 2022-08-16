f27013bf26046903c201d0052f9ae4b2c94f3917
Koda <s_riku@shiolab.com>
2022-08-16 20:07:00 +0900

    メソッド名の変更と結果表示の処理変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,24 +3,21 @@
 WIDTH = 3
 
 def main
-  result(new_file_lists)
+  show_result(make_filelists)
 end
 
-def new_file_lists
+def make_filelists
   file_names = Dir.glob('*')
   file_names = file_names.each_slice((file_names.size / WIDTH.to_f).ceil).to_a
   max_size = file_names.map(&:size).max
   file_names.map { |it| it.values_at(0...max_size) }.transpose
 end
 
-def result(file_lists)
-  j = 0
-  file_lists.size.times do |i|
-    while j < WIDTH
-      print(file_lists[i][j].to_s.ljust(15))
-      j += 1
+def show_result(file_lists)
+  file_lists.each do |list|
+    list.each do |name|
+      print(name.to_s.ljust(15))
     end
-    j = 0
     puts
   end
 end

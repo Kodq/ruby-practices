ca14042a83c83d90ad8d19cc01c284a3c5bc5be1
Koda <s_riku@shiolab.com>
2022-08-20 17:12:35 +0900

    メソッド名と処理を簡単に

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,10 +3,10 @@
 WIDTH = 3
 
 def main
-  show_result(make_filelists)
+  show_result(make_file_lists)
 end
 
-def make_filelists
+def make_file_lists
   file_names = Dir.glob('*')
   file_names = file_names.each_slice((file_names.size / WIDTH.to_f).ceil).to_a
   max_size = file_names.map(&:size).max
@@ -15,10 +15,7 @@ end
 
 def show_result(file_lists)
   file_lists.each do |list|
-    list.each do |name|
-      print(name.to_s.ljust(15))
-    end
-    puts
+    puts(list.map { |name| name.to_s.ljust(15) }.join)
   end
 end
 

5473e5b02790cb319fb4327715e4ba5606fd0823
Koda <s_riku@shiolab.com>
2023-01-24 00:58:08 +0900

    レビュー箇所の修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,9 +3,14 @@
 require 'optparse'
 
 def main
-  file_names = fetch_name
   option = fetch_option
-  pipe = File.pipe?($stdin)
+  inputs = $stdin.readlines
+
+  input_names = if inputs.empty?
+                  fetch_file
+                else
+                  inputs
+                end
 
   if option.empty?
     option[:lines] = true
@@ -13,11 +18,65 @@ def main
     option[:chars] = true
   end
 
-  show_stats(file_names, option, pipe)
+  show_stats(input_names, inputs, option)
 end
 
-def fetch_name
-  $stdin.readlines
+def show_stats(input_names, inputs, option)
+  total_infos = { lines: 0, words: 0, bytes: 0 }
+  select_option = option.select { |_k, v| v == true }
+
+  saves_info = []
+
+  input_names.each do |name|
+    file_infos = []
+
+    input_contents = argv_of_input(name, inputs)
+
+    file_infos.push(show_lines(input_contents))
+    file_infos.push(show_words(input_contents))
+    file_infos.push(show_bytes(name, inputs))
+
+    total_infos[:lines] = file_infos[0]
+    total_infos[:words] = file_infos[1]
+    total_infos[:chars] = file_infos[2]
+
+    if inputs.empty?
+      select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
+      puts(name)
+    end
+
+    saves_info.push(file_infos)
+  end
+
+  show_sum(saves_info, inputs, option) if saves_info.size >= 2
+end
+
+def show_sum(saves_info, inputs, option)
+  swap_infos = saves_info.transpose
+  select_option = option.select { |_k, v| v == true }
+  total_infos = { lines: 0, words: 0, chars: 0 }
+
+  total_infos[:words] += swap_infos[1].sum
+  total_infos[:chars] += swap_infos[2].sum
+  total_infos[:lines] += inputs.empty? ? swap_infos[0].sum : swap_infos[0].size
+  select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
+  print 'Total' if inputs.empty?
+end
+
+def fetch_file
+  file_names = ARGV
+  file_names.pop unless fetch_option.empty?
+  file_names
+end
+
+def argv_of_input(name, inputs)
+  if inputs.empty?
+    name = name.chomp
+    input_contents = File.read(name)
+  else
+    input_contents = name
+  end
+  input_contents
 end
 
 def fetch_option
@@ -27,54 +86,8 @@ def fetch_option
   opt.on('-w') { |w| option[:words] = w }
   opt.on('-c') { |c| option[:chars] = c }
   opt.parse(ARGV)
+
   option
-end
-
-def show_stats(file_names, option, pipe)
-  total_infos = { lines: 0, words: 0, bytes: 0 }
-  select_option = option.select { |_k, v| v == true }
-  input_contents = File.read(name)
-
-  saves_info = []
-  file_names.each do |name|
-    file_infos = []
-    name = name.chomp
-
-    file_infos.push(show_lines(input_contents))
-    file_infos.push(show_words(input_contents))
-    file_infos.push(show_bytes(name, pipe))
-
-    total_infos[:lines] = file_infos[0]
-    total_infos[:words] = file_infos[1]
-    total_infos[:chars] = file_infos[2]
-
-    unless pipe
-      select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
-      puts(name)
-    end
-
-    saves_info.push(file_infos)
-  end
-
-  show_sum(saves_info, pipe, option) if saves_info.size >= 2
-end
-
-def show_sum(saves_info, pipe, option)
-  swap_infos = saves_info.transpose
-  select_option = option.select { |_k, v| v == true }
-  total_infos = { lines: 0, words: 0, chars: 0 }
-
-  total_infos[:words] += swap_infos[1].sum
-  total_infos[:chars] += swap_infos[2].sum
-
-  if pipe
-    total_infos[:lines] += swap_infos[0].size
-    select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
-  else
-    total_infos[:lines] += swap_infos[0].sum
-    select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
-    print('Total')
-  end
 end
 
 def show_lines(input_contents)
@@ -85,11 +98,11 @@ def show_words(inputs_contents)
   inputs_contents.split(' ').size
 end
 
-def show_bytes(file_name, pipe)
-  if pipe
-    file_name.bytesize
+def show_bytes(name, inputs)
+  if inputs.empty?
+    File.size(name)
   else
-    File.size(file_name)
+    name.bytesize
   end
 end
 

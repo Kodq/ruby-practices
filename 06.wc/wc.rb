4a7ca01ceb271915e4a8b7707f1b53b386bee7ad
Koda <s_riku@shiolab.com>
2023-01-31 22:44:43 +0900

    動作確認見直し、各問題の修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,12 +4,13 @@ require 'optparse'
 
 def main
   option = fetch_option
-  inputs = $stdin.readlines
 
-  input_names = if inputs.empty?
+  argv_lists = ARGV
+
+  input_names = if argv_lists.empty?
+                  $stdin.readlines
+                elsif File.exist?(argv_lists.first)
                   fetch_file(option)
-                else
-                  inputs
                 end
 
   if option.values.none?
@@ -18,10 +19,10 @@ def main
     option[:chars] = true
   end
 
-  show_stats(input_names, inputs, option)
+  show_stats(input_names, option, argv_lists)
 end
 
-def show_stats(input_names, inputs, option)
+def show_stats(input_names, option, argv_lists)
   total_infos = { lines: 0, words: 0, chars: 0 }
   select_option = option.select { |_k, v| v }
 
@@ -30,17 +31,17 @@ def show_stats(input_names, inputs, option)
   input_names.each do |name|
     file_infos = []
 
-    input_contents = argv_of_input(name, inputs)
+    input_contents = argv_or_input(name, argv_lists)
 
     file_infos.push(show_lines(input_contents))
     file_infos.push(show_words(input_contents))
-    file_infos.push(show_bytes(name, inputs))
+    file_infos.push(show_bytes(name, argv_lists))
 
     total_infos[:lines] = file_infos[0]
     total_infos[:words] = file_infos[1]
     total_infos[:chars] = file_infos[2]
 
-    if inputs.empty?
+    unless argv_lists.empty?
       select_option.each_key { |k| print("#{total_infos.values_at(k)[0]} ") }
       puts(name)
     end
@@ -48,33 +49,28 @@ def show_stats(input_names, inputs, option)
     saves_info.push(file_infos)
   end
 
-  show_sum(saves_info, inputs, option) if saves_info.size >= 2
+  show_sum(saves_info, argv_lists, option) if (saves_info.size >= 2) || argv_lists.empty?
 end
 
-def show_sum(saves_info, inputs, option)
-  swap_infos = saves_info.transpose
+def show_sum(save_infos, argv_lists, option)
+  swap_infos = save_infos.transpose
   select_option = option.select { |_k, v| v }
   total_infos = { lines: 0, words: 0, chars: 0 }
 
   total_infos[:words] += swap_infos[1].sum
   total_infos[:chars] += swap_infos[2].sum
-  total_infos[:lines] += inputs.empty? ? swap_infos[0].sum : swap_infos[0].size
+  total_infos[:lines] += argv_lists.empty? ? swap_infos[0].size : swap_infos[0].sum
+
   select_option.each_key { |key| print("#{total_infos.values_at(key)[0]} ") }
-  print 'Total' if inputs.empty?
+  print 'Total' unless argv_lists.empty?
 end
 
-def fetch_file(option)
-  file_names = ARGV
-  file_names.pop if option.values.any?
-  file_names
-end
-
-def argv_of_input(name, inputs)
-  if inputs.empty?
+def argv_or_input(name, argv_lists)
+  if argv_lists.empty?
+    input_contents = name
+  else
     name = name.chomp
     input_contents = File.read(name)
-  else
-    input_contents = name
   end
   input_contents
 end
@@ -87,6 +83,8 @@ def fetch_option
   opt.on('-c') { |c| option[:chars] = c }
   opt.parse(ARGV)
 
+  ARGV.pop if option.values.any?
+
   option
 end
 
@@ -98,11 +96,11 @@ def show_words(inputs_contents)
   inputs_contents.split(' ').size
 end
 
-def show_bytes(name, inputs)
-  if inputs.empty?
-    File.size(name)
-  else
+def show_bytes(name, argv_lists)
+  if argv_lists.empty?
     name.bytesize
+  else
+    File.size(name)
   end
 end
 
